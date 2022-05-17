9ba19541dfcf14c5ed738fa6a71cbcde433c498d
uxri <yuree0217@gmail.com>
2022-05-17 23:08:21 +0900

    [FEAT] #2 - Reflect Code Review, Modify XML

--- app/src/main/java/org/sopt/daangnmarket_android/ui/view/write/WriteActivity.kt
+++ app/src/main/java/org/sopt/daangnmarket_android/ui/view/write/WriteActivity.kt
@@ -14,21 +14,18 @@ class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_write)
-        //버튼을 눌러도 아무일이 일어나지 않고 있습니다......... 혹시 제가 바보같은 짓을 했나요?
+        setContentView(binding.root)
 
         binding.btnFinish.setOnClickListener {
             if (binding.etTitle.text.isNullOrBlank() || binding.etPrice.text.isNullOrBlank() || binding.etContent.text.isNullOrBlank()) {
                 Toast.makeText(this, "채워지지 않은 부분이 있습니다", Toast.LENGTH_SHORT).show()
-                //이렇게 쓴게 맞나요?
             } else {
-                //완료버튼 주황색으로 변경해야하는데... ㅠㅠ .. 함수를 뭘 써야할지 모르겠음...
-                finish()
+                startActivity(Intent(this@WriteActivity, MainActivity::class.java))
+                //우선은 버튼 변화 없이 MainActivity로 연결해두었습니다.
             }
         }
-        binding.btnBack.setOnClickListener() {
-                startActivity(Intent(this@WriteActivity, MainActivity::class.java))
-            //이게 맞나요
+        binding.btnBack.setOnClickListener {
+            finish()
         }
     }
 }
