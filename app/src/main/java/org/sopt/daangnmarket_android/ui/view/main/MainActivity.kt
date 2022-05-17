e859a70d663c4089d106851ead967389f1dd59e1
hansh0101 <hansh0101@naver.com>
2022-05-17 09:50:59 +0900

    Initial commit

--- app/src/main/java/org/sopt/daangnmarket_android/ui/view/main/MainActivity.kt
+++ app/src/main/java/org/sopt/daangnmarket_android/ui/view/main/MainActivity.kt
@@ -1,8 +1,10 @@
-package org.sopt.daangnmarket_android
+package org.sopt.daangnmarket_android.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.daangnmarket_android.R
 
+// 다희
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/org/sopt/daangnmarket_android/ui/view/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.daangnmarket_android.ui.view.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.daangnmarket_android.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
