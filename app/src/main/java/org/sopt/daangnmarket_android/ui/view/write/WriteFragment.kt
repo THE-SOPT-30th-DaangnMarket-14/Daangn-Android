ec6faf768cfe511d4316d55fce4a0e10dd761202
uxri <yuree0217@gmail.com>
2022-05-22 22:39:36 +0900

    [FEAT] #2 Viewbinding

--- app/src/main/java/org/sopt/daangnmarket_android/ui/view/write/WriteFragment.kt
+++ app/src/main/java/org/sopt/daangnmarket_android/ui/view/write/WriteFragment.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
