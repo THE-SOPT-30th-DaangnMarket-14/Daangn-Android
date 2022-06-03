38ed406c6a7a4ebd34fb652576adbdb4383292f3
hansh0101 <hansh0101@naver.com>
2022-06-03 10:17:43 +0900

    [FEAT] #13 - base url 'http://' 추가

--- app/src/main/java/org/sopt/daangnmarket_android/data/ServiceCreator.kt
+++ app/src/main/java/org/sopt/daangnmarket_android/data/ServiceCreator.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceCreator {
-    private const val BASE_URL = "52.78.114.176:8000"
+    private const val BASE_URL = "http://52.78.114.176:8000"
 
     private val retrofit: Retrofit = Retrofit.Builder() //생성자 호출
         .baseUrl(BASE_URL) //서버에 메인 URL 전달
